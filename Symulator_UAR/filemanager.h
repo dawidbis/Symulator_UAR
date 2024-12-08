fbc8ad550bab65be1d146e7a3a341057e0107889
macbondyra <mb310470@student.polsl.pl>
2024-12-08 12:21:43 +0100

    usuniete glupoty z filemanagera

--- Symulator_UAR/filemanager.h
+++ Symulator_UAR/filemanager.h
@@ -31,34 +31,8 @@ private slots:
 signals:
 
 
-private:
-    //Do obsługi instancji dodalem
+private:   
     loopsystem* loopSystem;
-    //ARX
-    double m_yiFM;
-    std::vector<double> m_AFM;
-    std::vector<double> m_BFM;
-    std::deque<double> m_opoznienieBuforFM;
-    std::deque<double> m_buforWejFM;
-    std::deque<double> m_buforWyjFM;
-    int m_kFM;
-    int m_iFM;
-    double m_ziFM;
-    //PID
-    double kPFM;
-    double tIFM;
-    double tDFM;
-    double maxUchybyFM;
-    double sumaUchybowFM;
-    double uchybPoprzedniFM;
-    bool antiWindupWlaczonyFM;
-    //LoopSystem
-    bool loopState;
-    double wartoscARX;
-    double wartoscPID;
-
-
-
 };
 
 #endif // FILEMANAGER_H
